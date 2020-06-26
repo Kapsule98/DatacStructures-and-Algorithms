c0de51436ed0622868a5f918b945b989cc61fa36
Kapil Bedekar <kapil9787@gmail.com>
2020-06-27 03:32:49 +0530

    Lowest common ancestor

--- Q6  LCA_try2.cpp
+++ Q6  LCA_try2.cpp
@@ -1,3 +1,11 @@
+/* Author : Kapil
+--------------------------------
+    Lowest common ancestor
+--------------------------------
+Time : O(N)
+Extra space : none
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
